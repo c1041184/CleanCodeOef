55a685997ea3b7aceb71d368f33cda5849bfc7de
Vincent <c1041184@hik.be>
2018-11-25 13:46:32 +0100

    Oef5

---
+++ CleanCodeOef/CleanCodeOef/Oef5.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeOef
+{
+    class Oef5
+    {
+
+        //public class Bird
+        //{
+        //    public virtual void SetLocation(double longitude, double latitude)
+        //    {
+        //    }
+        //    public virtual void SetAltitude(double altitude)
+        //    {
+        //    }
+        //    public virtual void Draw()
+        //    {
+        //    }
+        //}
+
+
+
+        public abstract class Bird
+        {
+            public virtual void SetLocation(double longitude, double latitude)
+            {
+            }
+
+            public virtual void Draw()
+            {
+            }
+        }
+
+        public abstract class FlyingBird : Bird
+        {
+            public virtual void SetAltitude(double altitude)
+            {
+            }
+        }
+
+        public class Arend : FlyingBird {
+        }
+
+        public class Merel : FlyingBird {
+        }
+
+        public class Struisvogel : Bird {
+        }
+
+    }
+}
